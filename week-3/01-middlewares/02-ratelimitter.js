3dc126d870adcaf0a6a1d076ba30860f877fa1de
Harsh Chandwani <harshchandwani50028@gmail.com>
2024-01-08 13:51:31 +0530

    Week 1 and 2 Complete

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -12,10 +12,27 @@ const app = express();
 // clears every one second
 
 let numberOfRequestsForUser = {};
+
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
-
+function globalMiddleware(req, res, next){
+  const userId = req.headers["user-id"];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("no entry");
+    }
+    else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+}
+app.use(globalMiddleware);
+app.use(express.json());
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
